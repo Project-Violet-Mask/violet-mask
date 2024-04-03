11f3a803a4a48b6bafd856578239a538aba8ee16
Trevor Coughlin <coughlin.156@osu.edu>
2024-04-02 20:11:30 -0400

    configured the Api with storecontext

--- src/api/violet.mask.Api/CatalogController.cs
+++ src/api/violet.mask.Api/CatalogController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using violet.mask.Domain.Catalog;
+using violet.mask.Data;
 
 namespace violet.mask.Api.Controllers
 {
@@ -7,18 +8,18 @@ namespace violet.mask.Api.Controllers
     [Route("api/[controller]")]
     public class CatalogController : ControllerBase
     {
+        private readonly StoreContext _context;
+
+        public CatalogController(StoreContext context)
+        {
+            _context = context;
+        }
+        
         [HttpGet]
         public IActionResult GetItems()
         {
-            var items = new[]
-            {
-                new Item(name: "Item 1", description: "Description", brand:"Brand 1", price: 100.00m),
-                new Item(name: "Item 2", description: "Description", brand:"Brand 2", price: 200.00m),
-                new Item(name: "Item 3", description: "Description", brand:"Brand 3", price: 300.00m),
-                new Item(name: "Item 4", description: "Description", brand:"Brand 4", price: 400.00m),
-                new Item(name: "Item 5", description: "Description", brand:"Brand 5", price: 500.00m),
-            };
-            return Ok(items);
+           
+            return Ok(_context.Items);
         }
 
         [HttpGet("{id:int}")]
